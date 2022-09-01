ab210ab6503a63985e2b6202d0c951e22204f5dd
SeonSo <tjsgh4500@gmail.com>
2022-09-01 09:43:52 +0900

    v0.0.0

--- oneday-piece/src/main/java/com/example/onedaypiece/challenge/dao/ChallengeRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.onedaypiece.challenge.dao;
-
-import com.example.onedaypiece.challenge.domain.challengeRecord.ChallengeRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChallengeRecordRepository extends JpaRepository<ChallengeRecord, Long> {
-
-}
